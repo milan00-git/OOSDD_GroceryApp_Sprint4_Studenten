ab46dec8d00a64b94ed10ab2da39dbb327b53105
Milan <mp09092003@gmail.com>
2025-10-03 09:13:12 +0200

    standaard Role is None

--- Grocery.Core/Services/BoughtProductsService.cs
+++ Grocery.Core/Services/BoughtProductsService.cs
@@ -1,5 +1,4 @@
-﻿
-using Grocery.Core.Interfaces.Repositories;
+﻿using Grocery.Core.Interfaces.Repositories;
 using Grocery.Core.Interfaces.Services;
 using Grocery.Core.Models;
 
@@ -20,7 +19,51 @@ namespace Grocery.Core.Services
         }
         public List<BoughtProducts> Get(int? productId)
         {
-            throw new NotImplementedException();
+            var result = new List<BoughtProducts>();    // Nieuwe lijst voor Client met: client, boodschappenlijst & product
+
+            // Een lege lijst geven als er geen product is
+            if (productId == null)
+            {
+                return result;
+            }
+
+            var product = _productRepository.Get(productId.Value);
+            if (product == null) return result;
+
+            // Alle items ophalen
+            var allItems = _groceryListItemsRepository.GetAll();
+
+            // Kijken per product of de hoeveelheid > 0
+            var gefilterdeItems = new List<GroceryListItem>();
+            foreach (var item in allItems)
+            {
+                if (item.ProductId == product.Id && item.Amount > 0)
+                {
+                    gefilterdeItems.Add(item);
+                }
+            }
+
+            var uniekeListIds = new List<int>();
+            foreach (var item in gefilterdeItems)
+            {
+                if (!uniekeListIds.Contains(item.GroceryListId))
+                {
+                    uniekeListIds.Add(item.GroceryListId);
+                }
+            }
+
+            foreach (var listId in uniekeListIds)
+            {
+                var list = _groceryListRepository.Get(listId);
+                if (list == null) continue;
+
+                var client = _clientRepository.Get(list.ClientId);
+                if (client == null) continue;
+
+                result.Add(new BoughtProducts(client, list, product));
+            }
+
+            return result;
         }
     }
 }
